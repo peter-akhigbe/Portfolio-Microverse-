00c6cff821f5265f7230c30a3bf460d09ff04b78
Peter Akhigbe <akhigbe37@gmail.com>
2023-02-08 18:33:28 +0100

    disable click on background in mobile nav

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('#menuBtn');
 const mobileNav = document.querySelector('#mobileNav');
 const body = document.querySelector('body');
+const closeBtn = document.querySelector('#closeBtn');
+const mobileNavLinks = document.querySelector('.mobileNavLinks');
 
 menuBtn.addEventListener('click', () => {
   mobileNav.style.height = '100vh';
@@ -8,7 +10,12 @@ menuBtn.addEventListener('click', () => {
   body.style.overflow = 'hidden';
 });
 
-mobileNav.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
+  mobileNav.style.height = '0px';
+  body.style.overflow = 'visible';
+});
+
+mobileNavLinks.addEventListener('click', () => {
   mobileNav.style.height = '0px';
   body.style.overflow = 'visible';
 });
